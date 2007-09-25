b6155bf23eed9bfc1e07ca2db60fb556932b82a8
cikeson <cikeson@99ca2c8f-4d30-0410-a16a-c50513983d21>
2007-09-25 03:18:21 +0000

    Added ID3v2 (2.x) support to the MPEGAudioFile parser. Improved the genre parsing for ID3v2 tags for the MPEGAudioFile parser. Complete refactored determining metadata from file path for the AudioFile class. These changes should result in more accurate metadata from now on.

--- trunk/jem-plugin-source-fs/src/main/java/net/kodeninja/jem/server/content/mimetypes/AudioFile.java
+++ trunk/jem-plugin-source-fs/src/main/java/net/kodeninja/jem/server/content/mimetypes/AudioFile.java
@@ -12,116 +12,72 @@ public class AudioFile extends GenericFile {
 		boolean addArtist = true;
 		boolean addTitle = true;
 		boolean addTrack = true;
-		boolean addAlbum = true;
 
 		for (Metadata metadata: item.getMetadataList()) {
-			addArtist &= (metadata.getType().equals(MetadataType.Format) == false);
+			addArtist &= (metadata.getType().equals(MetadataType.Artist) == false);
 			addTitle &= (metadata.getType().equals(MetadataType.Title) == false);
 			addTrack &= (metadata.getType().equals(MetadataType.SetPosition) == false);
-			addAlbum &= (metadata.getType().equals(MetadataType.Set) == false);
 
 			if ((addArtist && addTitle && addTrack) == false)
 				break;
 		}
 
-		int pos, pos2 = -2;
-		String origFilename = item.getURI().getPath().trim().replace('_', ' ').replaceAll("%20", " ");
-		String filename = origFilename;
+		if (addArtist || addTitle || addTrack) {
+			String track = null;
+			String artist = null;
+			String title = null;
+			String file = item.getURI().toString().replace('_', ' ').replaceAll("%20", " ");
+			file = file.substring(file.lastIndexOf("/") + 1);
+			file = file.substring(0, file.lastIndexOf("."));
 
-		//Trim off the filename junk
-		if ((pos = filename.lastIndexOf("/")) > -1)
-			filename = filename.substring(pos + 1);
-		if ((pos = filename.lastIndexOf(".")) > -1)
-			filename = filename.substring(0, pos);
-
-		int track = -1;
-		//Get the first space
-		int pos3 = filename.indexOf(" ");
-
-		if ((pos3 == 2) || ((pos3 == 3) && (filename.charAt(2) == '.'))) { // "## Rest" or "##. Rest" format
-			String tmp = filename.substring(0, 2).trim();
-			try {
-				track = Integer.parseInt(tmp);
-				if (filename.charAt(2) == '.')
-					filename = filename.substring(3).trim();
-				else
-					filename = filename.substring(2).trim();
-			}
-			catch (NumberFormatException e) {}
-		}
-		else if (filename.toLowerCase().startsWith("track ")) { // "Track ## Rest" or "Track ##. Rest"
-			String tmp = filename.substring(6, 8).trim();
-			try {
-				track = Integer.parseInt(tmp);
-				if (filename.charAt(8) == '.')
-					filename = filename.substring(9).trim();
-				else
-					filename = filename.substring(8).trim();
-			}
-			catch (NumberFormatException e) {}
-		}
-
-		pos = 0;
-		while (pos == 0) { //Ignore - at the very beginning of filenames
-			if ((pos = filename.indexOf(" - ")) == -1) {
-				pos = filename.indexOf("-");
-
-				if ((pos > 0) && (pos < filename.length() - 1))
-					pos2 = pos + 1;
-				else
-					pos = -1;
-			}
-			else //Move the secondary position (song title) beyond the name
-				pos2 = pos + 2;
-
-			if (pos == 0) //Cut the - from the beginning
-				filename = filename.substring(1);
-		}
-
-		if (pos == filename.length()) { //Fixes - at end of filename bug
-			filename = filename.substring(0, filename.length() - 1);
-			pos = -1;
-		}
-
-
-		String title = null;
-		String album = null;
-		String artist = null;
-
-		if (pos == -1) { // "Title" format
-			title = filename;
-		}
-		else { // "Artist - Title"
-			artist = filename.substring(0, pos).trim();
-			title = filename.substring(pos2 + 1);
-		}
-
-
-		if (track >= 0) {
-			String tmp = origFilename.substring(0, origFilename.lastIndexOf("/"));
-			pos = tmp.lastIndexOf("/");
-			if (pos > 0) {
-				album = tmp.substring(pos + 1, tmp.length());
-				if ((artist == null) || (artist.length() == 0)) {
-					tmp = tmp.substring(0, pos);
-					pos = tmp.lastIndexOf("/");
-					if (pos > 0)
-						artist = tmp.substring(pos + 1, tmp.length());
+			int start = 0;
+			for (int i = 0; i < file.length(); i++) {
+				if (start == 0) {
+					if ((file.charAt(i) == '.') || (file.charAt(i) == ' ')) { // Catch ##. Rest or ## Rest
+						track = file.substring(0, i);
+						try {
+							int tmp = Integer.parseInt(track);
+							if (tmp > 99)
+								tmp = tmp % 100;
+							track = "" + tmp;
+							start = i + 1;
+							continue;
+						}
+						catch (NumberFormatException e) {
+							track = null;
+						}
+					}
+				}
+				if ((file.length() > i + 3) && file.substring(i, i + 3).equals(" - ") && (artist == null)) { // Catch Artist - Title
+					artist = file.substring(start, i);
+					title = file.substring(i + 3);
+					break;
 				}
 			}
+			if ((artist == null) && (title == null)) { // Catch Title
+				title = file.substring(start);
+			}
+			else if (title.toLowerCase().startsWith("track")) { // Catch Artist - Track ## 
+				track = title.substring(5).trim();
+				try {
+					int tmp = Integer.parseInt(track);
+					track = "" + tmp;
+				}
+				catch (NumberFormatException e) {
+					track = null;
+				}
+			}
+
+
+			if ((addArtist) && (artist != null) && (artist.length() > 0))
+				JemServer.getMediaStorage().addMediaMetadata(item, MetadataType.Artist, artist.trim());
+
+			if ((addTitle) && (title != null) && (title.length() > 0))
+				JemServer.getMediaStorage().addMediaMetadata(item, MetadataType.Title, title.trim());
+
+			if ((addTrack) && (track != null) && (track.length() > 0))
+				JemServer.getMediaStorage().addMediaMetadata(item, MetadataType.SetPosition, track.trim());
 		}
-
-		if ((addArtist) && (artist != null) && (artist.length() > 0))
-			JemServer.getMediaStorage().addMediaMetadata(item, MetadataType.Artist, artist);
-
-		if ((addTitle) && (title != null) && (title.length() > 0))
-			JemServer.getMediaStorage().addMediaMetadata(item, MetadataType.Title, title);
-
-		if ((addAlbum) && (album != null) && (album.length() >= 0))
-			JemServer.getMediaStorage().addMediaMetadata(item, MetadataType.Set, album);
-
-		if ((addTrack) && (track >= 0))
-			JemServer.getMediaStorage().addMediaMetadata(item, MetadataType.SetPosition, "" + track);
 
 		super.addMetadata(item);
 	}
